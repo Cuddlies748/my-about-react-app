79ff6e626ea18d9928bb30a6c797b95b48d838d6
Jasur0201 <liderpro567@gmail.com>
2025-03-01 15:07:54 +0300

    Обновил проект

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import About from './components/About';
-import './App.css';
-import Aboutme from './components/Aboutme';
-import Projects from './components/Projects';
-import CustomCursor from './components/CustomCursor';
+import About from "./components/About";
+import "./App.css";
+import Aboutme from "./components/Aboutme";
+import Projects from "./components/Projects";
+import CustomCursor from "./components/CustomCursor";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [showComponent, setShowComponent] = useState(window.innerWidth > 480);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setShowComponent(window.innerWidth > 480);
+    };
+
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <div className="app-container">
-      <CustomCursor />
       <About />
       <Aboutme />
       <Projects />
+      {showComponent && <CustomCursor />}
     </div>
   );
 }
