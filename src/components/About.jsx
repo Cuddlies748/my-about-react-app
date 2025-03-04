86a042a70a81871ce085efcdf2280c2d36b85996
Jasur0201 <liderpro567@gmail.com>
2025-03-04 15:39:16 +0300

    Обновил проект

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,17 @@
-
-import "../styles/about.css"
+import "../styles/about.css";
 import React, { useEffect, useRef } from "react";
+
 function About() {
   const starsContainerRef = useRef(null);
+  const maxStars = 100; // Ограничиваем общее количество звезд
 
   useEffect(() => {
     function createStar() {
       if (!starsContainerRef.current) return;
+
+      if (starsContainerRef.current.children.length >= maxStars) {
+        return; // Если звезд уже слишком много, новые не создаём
+      }
 
       const star = document.createElement("div");
       star.classList.add("star");
@@ -26,26 +31,37 @@ function About() {
     }
 
     const interval = setInterval(() => {
-      for (let i = 0; i < 15; i++) createStar(); // Создаём 20 звёзд за раз
-    }, 200); // Каждые 200 мс
-
+      for (let i = 0; i < 20; i++) {
+        createStar(); // Создаём 20 звезд за раз
+      }
+    }, 300); // Каждые 300 мс
+    
     return () => clearInterval(interval);
   }, []);
+
   return (
     <div data-ignore-cursor="true" className="About">
-      <div data-ignore-cursor="true" className="stars-container" ref={starsContainerRef}></div>
-      <div data-ignore-cursor="true" className='text_div'>
-      <h2 data-ignore-cursor="true" className='about_h2'>About Me</h2>
-      
-  <h2 data-ignore-cursor="true" class="text-container">Eshboev Jasur</h2>
+      <div
+        data-ignore-cursor="true"
+        className="stars-container"
+        ref={starsContainerRef}
+      ></div>
+      <div data-ignore-cursor="true" className="text_div">
+        <h2 data-ignore-cursor="true" className="about_h2">
+          About Me
+        </h2>
+        <h2 data-ignore-cursor="true" className="text-container">
+          Eshboev Jasur
+        </h2>
       </div>
-      <a className="button-86" href=" https://cuddlies748.github.io/my-react-app/">Back to home
+      <a
+        className="button-86"
+        href=" https://cuddlies748.github.io/my-react-app/"
+      >
+        Back to home
       </a>
-  
-</div>
-   
+    </div>
   );
 }
 
 export default About;
-
